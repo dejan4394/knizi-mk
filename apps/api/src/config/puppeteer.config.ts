7836d29ed70785f902d7524c759d9fca3f79e730
dejan4394 <dejanmalinov@yahoo.com>
2026-05-28 14:56:15 +0200

    Финална точна патека за Puppeteer Chrome од логовите

--- apps/api/src/config/puppeteer.config.ts
+++ apps/api/src/config/puppeteer.config.ts
@@ -6,7 +6,6 @@ export const puppeteerConfig = {
     process.env.NODE_ENV === 'production' ||
     process.env.PUPPETEER_SKIP_CHROMIUM_DOWNLOAD === 'true',
 
-  // Ова е вистинската апсолутна патека каде Render го крие извршниот Chrome по инсталацијата!
   executablePath:
-    '/opt/render/project/src/.cache/puppeteer/chrome/linux-148.0.7778.97/chrome-linux64/chrome',
+    '/opt/render/.cache/puppeteer/chrome/linux-148.0.7778.97/chrome-linux64/chrome',
 };

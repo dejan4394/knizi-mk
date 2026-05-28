971ed6add2be06a3797a057b67b6c18b978fd458
dejan4394 <dejanmalinov@yahoo.com>
2026-05-28 12:51:21 +0200

    Ажуриран CORS за Vercel

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3000', // Патеката на Next.js
+    origin: ['http://localhost:3000', /\.vercel\.app$/],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
